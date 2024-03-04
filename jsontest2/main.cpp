73e25af2f8033e4b8eb865d7bc61327f1833197c
evgeniy.syrygin@auslogics.com <evgeniy.syrygin@auslogics.com>
2024-03-04 15:02:56 +0600

    test2

---
+++ jsontest2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
